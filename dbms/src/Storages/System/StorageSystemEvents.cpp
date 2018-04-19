79819bc89943e3ed579d670231d11ee73128780d
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2018-04-19 18:42:55 +0300

    fix build

--- dbms/src/Storages/System/StorageSystemEvents.cpp
+++ dbms/src/Storages/System/StorageSystemEvents.cpp
@@ -24,7 +24,7 @@ StorageSystemEvents::StorageSystemEvents(const std::string & name_)
 BlockInputStreams StorageSystemEvents::read(
     const Names & column_names,
     const SelectQueryInfo &,
-    const Context &,
+    const Context & context,
     QueryProcessingStage::Enum processed_stage,
     const size_t /*max_block_size*/,
     const unsigned /*num_streams*/)
